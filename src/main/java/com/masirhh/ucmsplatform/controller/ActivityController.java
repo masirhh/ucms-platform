903371ee490b3f547863455884e377854cb626b4
Masirhh <masirhh@outlook.com>
2019-12-03 15:05:45 +0800

    [add] add ActivityController methods

--- src/main/java/com/masirhh/ucmsplatform/controller/ActivityController.java
+++ src/main/java/com/masirhh/ucmsplatform/controller/ActivityController.java
@@ -5,6 +5,7 @@ import com.masirhh.ucmsplatform.domain.Activity;
 import com.masirhh.ucmsplatform.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +21,11 @@ public class ActivityController {
     public R<List<Activity>> getActivity() {
         List<Activity> list = activityService.list();
         return R.ok(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public R<Activity> getActivityDetail(@PathVariable(value = "id")Long id){
+        Activity byId = activityService.getById(id);
+        return R.ok(byId);
     }
 }
