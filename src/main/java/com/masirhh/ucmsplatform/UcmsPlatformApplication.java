112c14160d5cda89f31a21faa5712a29a223b732
masirhh <masirhh@outlook.com>
2019-12-05 21:12:27 +0800

    [add] add nacos cloud

--- src/main/java/com/masirhh/ucmsplatform/UcmsPlatformApplication.java
+++ src/main/java/com/masirhh/ucmsplatform/UcmsPlatformApplication.java
@@ -3,9 +3,11 @@ package com.masirhh.ucmsplatform;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("com.masirhh.ucmsplatform.mapper")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UcmsPlatformApplication {
 
     public static void main(String[] args) {
